82e8c3bc953909ca300545eaad22d6278f59e1ac
Jack's Style <117783266+jacks-style@users.noreply.github.com>
2023-02-23 00:11:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const CLICK_TXT = document.querySelector(".click-txt");
 console.log(BUTTONS);
 BUTTONS.forEach((button) => {
   button.addEventListener("click", () => {
-    CLICK_TXT.style.color = "rgb(0, 222, 4)";
+    CLICK_TXT.style.color = "rgb(12, 168, 199)";
   });
   button.addEventListener("mouseout", () => {
     CLICK_TXT.style.color = "rgb(255, 255, 255)";
