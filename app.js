64b72f6a9cc57b024d4d210365e248cad8d4bba9
Jack's Style <117783266+jacks-style@users.noreply.github.com>
2023-02-28 14:55:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const BUTTONS = document.querySelectorAll("button");
 const CLICK_TXT = document.querySelector(".click-txt");
-console.log(BUTTONS);
 BUTTONS.forEach((button) => {
   button.addEventListener("click", () => {
     CLICK_TXT.style.color = "rgb(12, 168, 199)";
